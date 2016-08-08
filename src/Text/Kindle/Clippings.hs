11b585c818dd82cbd607b7174a18ba87c3a0ecc0
Eric Bailey <eric@ericb.me>
2016-08-08 03:06:18 -0500

    Ditch Haskell and example.txt; BSD-3 -> MIT

--- src/Text/Kindle/Clippings.hs
+++
@@ -1,7 +0,0 @@
-module Text.Kindle.Clippings (
-  module Text.Kindle.Clippings.Types,
-  module Text.Kindle.Clippings.Reader
-) where
-
-import Text.Kindle.Clippings.Types
-import Text.Kindle.Clippings.Reader
